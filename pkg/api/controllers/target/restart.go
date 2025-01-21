daytonaio/daytona

fix: restart methods (#1685)
Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

pkg/api/controllers/target/restart.go
@@ -1,0 +1,36 @@
+// Copyright 2024 Daytona Platforms Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package target
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/daytonaio/daytona/pkg/server"
+	"github.com/gin-gonic/gin"
+)
+
+// RestartTarget 			godoc
+//
+//	@Tags			target
+//	@Summary		Restart target
+//	@Description	Restart target
+//	@Param			targetId	path	string	true	"Target ID or Name"
+//	@Success		200
+//	@Router			/target/{targetId}/restart [post]
+//
+//	@id				RestartTarget
+func RestartTarget(ctx *gin.Context) {
+	targetId := ctx.Param("targetId")
+
+	server := server.GetInstance(nil)
+
+	err := server.TargetService.RestartTarget(ctx.Request.Context(), targetId)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("failed to restart target %s: %w", targetId, err))
+		return
+	}
+
+	ctx.Status(200)
+}

pkg/api/controllers/workspace/restart.go
@@ -1,0 +1,36 @@
+// Copyright 2024 Daytona Platforms Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package workspace
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/daytonaio/daytona/pkg/server"
+	"github.com/gin-gonic/gin"
+)
+
+// RestartWorkspace 			godoc
+//
+//	@Tags			workspace
+//	@Summary		Restart workspace
+//	@Description	Restart workspace
+//	@Param			workspaceId	path	string	true	"Workspace ID or Name"
+//	@Success		200
+//	@Router			/workspace/{workspaceId}/restart [post]
+//
+//	@id				RestartWorkspace
+func RestartWorkspace(ctx *gin.Context) {
+	workspaceId := ctx.Param("workspaceId")
+
+	server := server.GetInstance(nil)
+
+	err := server.WorkspaceService.RestartWorkspace(ctx.Request.Context(), workspaceId)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("failed to restart workspace %s: %w", workspaceId, err))
+		return
+	}
+
+	ctx.Status(200)
+}

pkg/services/target.go
@@ -19,6 +19,7 @@ type ITargetService interface {
 	ListTargets(ctx context.Context, filter *stores.TargetFilter, params TargetRetrievalParams) ([]TargetDTO, error)
 	StartTarget(ctx context.Context, targetId string) error
 	StopTarget(ctx context.Context, targetId string) error
+	RestartTarget(ctx context.Context, targetId string) error
 	SetDefault(ctx context.Context, targetId string) error
 	UpdateTargetProviderMetadata(ctx context.Context, targetId, metadata string) error
 	RemoveTarget(ctx context.Context, targetId string) error

pkg/services/workspace.go
@@ -18,6 +18,7 @@ type IWorkspaceService interface {
 	ListWorkspaces(ctx context.Context, params WorkspaceRetrievalParams) ([]WorkspaceDTO, error)
 	StartWorkspace(ctx context.Context, workspaceId string) error
 	StopWorkspace(ctx context.Context, workspaceId string) error
+	RestartWorkspace(ctx context.Context, workspaceId string) error
 	RemoveWorkspace(ctx context.Context, workspaceId string) error
 	ForceRemoveWorkspace(ctx context.Context, workspaceId string) error
 	UpdateWorkspaceProviderMetadata(ctx context.Context, workspaceId, metadata string) error

hanzoai/runtime

pkg/api/controllers/target/restart.go
@@ -1,0 +1,36 @@
+// Copyright 2024 Daytona Platforms Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package target
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/daytonaio/daytona/pkg/server"
+	"github.com/gin-gonic/gin"
+)
+
+// RestartTarget 			godoc
+//
+//	@Tags			target
+//	@Summary		Restart target
+//	@Description	Restart target
+//	@Param			targetId	path	string	true	"Target ID or Name"
+//	@Success		200
+//	@Router			/target/{targetId}/restart [post]
+//
+//	@id				RestartTarget
+func RestartTarget(ctx *gin.Context) {
+	targetId := ctx.Param("targetId")
+
+	server := server.GetInstance(nil)
+
+	err := server.TargetService.Restart(ctx.Request.Context(), targetId)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("failed to restart target %s: %w", targetId, err))
+		return
+	}
+
+	ctx.Status(200)
+}

pkg/api/controllers/workspace/restart.go
@@ -1,0 +1,36 @@
+// Copyright 2024 Daytona Platforms Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package workspace
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/daytonaio/daytona/pkg/server"
+	"github.com/gin-gonic/gin"
+)
+
+// RestartWorkspace 			godoc
+//
+//	@Tags			workspace
+//	@Summary		Restart workspace
+//	@Description	Restart workspace
+//	@Param			workspaceId	path	string	true	"Workspace ID or Name"
+//	@Success		200
+//	@Router			/workspace/{workspaceId}/restart [post]
+//
+//	@id				RestartWorkspace
+func RestartWorkspace(ctx *gin.Context) {
+	workspaceId := ctx.Param("workspaceId")
+
+	server := server.GetInstance(nil)
+
+	err := server.WorkspaceService.Restart(ctx.Request.Context(), workspaceId)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("failed to restart workspace %s: %w", workspaceId, err))
+		return
+	}
+
+	ctx.Status(200)
+}

pkg/services/target.go
@@ -19,6 +19,7 @@ type ITargetService interface {
 	Save(ctx context.Context, target *models.Target) error
 	Start(ctx context.Context, targetId string) error
 	Stop(ctx context.Context, targetId string) error
+	Restart(ctx context.Context, targetId string) error
 	SetDefault(ctx context.Context, targetId string) error
 	Delete(ctx context.Context, targetId string) error
 	ForceDelete(ctx context.Context, targetId string) error

pkg/services/workspace.go
@@ -20,6 +20,7 @@ type IWorkspaceService interface {
 	Stop(ctx context.Context, workspaceId string) error
 	Delete(ctx context.Context, workspaceId string) error
 	ForceDelete(ctx context.Context, workspaceId string) error
+	Restart(ctx context.Context, workspaceId string) error
 
 	UpdateMetadata(ctx context.Context, workspaceId string, metadata *models.WorkspaceMetadata) (*models.WorkspaceMetadata, error)
 	UpdateProviderMetadata(ctx context.Context, workspaceId, metadata string) error